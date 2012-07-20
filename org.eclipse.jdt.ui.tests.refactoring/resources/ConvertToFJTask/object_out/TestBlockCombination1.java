b8d63f05ccbb7e44852f5521ae9adb43b35fd609
Kyle Doren <doren1@illinois.edu>
2012-07-20 09:16:57 -0500

    Testing output for testingTrials

--- org.eclipse.jdt.ui.tests.refactoring/resources/ConvertToFJTask/object_out/TestBlockCombination1.java
+++ org.eclipse.jdt.ui.tests.refactoring/resources/ConvertToFJTask/object_out/TestBlockCombination1.java
@@ -1,1 +1,56 @@
 package object_out;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+public class TestBlockCombination1 {
+	
+	public int tryThis(int x) {
+		int processorCount = Runtime.getRuntime().availableProcessors();
+		ForkJoinPool pool = new ForkJoinPool(processorCount);
+		TryThisImpl aTryThisImpl = new TryThisImpl(x);
+		pool.invoke(aTryThisImpl);
+		return aTryThisImpl.result;
+	}
+
+	public class TryThisImpl extends RecursiveAction {
+		private int x;
+		private int result;
+		private TryThisImpl(int x) {
+			this.x = x;
+		}
+		protected void compute() {
+			if (x < 10) {
+				result = tryThis(x);
+				return;
+			} else {
+				if (x > 15) {
+					TryThisImpl task1 = new TryThisImpl(x - 12);
+					TryThisImpl task2 = new TryThisImpl(x - 15);
+					invokeAll(task1, task2);
+					int x1 = task1.result;
+					int x2 = task2.result;
+					result = x1 + x2;
+				} else {
+					int x3 = tryThis(x - 1);
+					result = x3;
+					return x3;
+				}
+			}
+		}
+		public int tryThis(int x) {
+			if (x < 0)
+				return 0;
+			else {
+				if (x > 15) {
+					int x1 = tryThis(x - 12);
+					int x2 = tryThis(x - 15);
+					return x1 + x2;
+				} else {
+					int x3 = tryThis(x - 1);
+					return x3;
+				}
+			}
+		}
+	}
+}
