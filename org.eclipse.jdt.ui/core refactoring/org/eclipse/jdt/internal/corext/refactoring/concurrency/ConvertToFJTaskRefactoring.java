0db1b4faecccc57d9de5815af46d5bcd05bce627
Kyle Doren <doren1@illinois.edu>
2012-08-27 10:04:18 -0500

    Removed TODO

--- org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/concurrency/ConvertToFJTaskRefactoring.java
+++ org.eclipse.jdt.ui/core refactoring/org/eclipse/jdt/internal/corext/refactoring/concurrency/ConvertToFJTaskRefactoring.java
@@ -290,7 +290,7 @@ public class ConvertToFJTaskRefactoring extends Refactoring {
 		Type returnType= fMethodDeclaration.getReturnType2();
 		String returnTypeName= returnType.resolveBinding().getName();
 		if (returnType.isPrimitiveType()) {
-			return ast.newSimpleType(ast.newSimpleName(primitiveTypeToWrapper(returnTypeName)));  //TODO Check if ASTNode util or something contains wrapper / boxing helper?
+			return ast.newSimpleType(ast.newSimpleName(primitiveTypeToWrapper(returnTypeName)));
 		} else if (returnType.isArrayType()) {
 			Type tempComponent= (Type) ASTNode.copySubtree(ast, ((ArrayType) returnType).getComponentType());
 			return ast.newArrayType(tempComponent);
